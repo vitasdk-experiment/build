58d41879f89f8cf1d69cc97cd8450fe4e733f9bf
173210 <root.3.173210@live.com>
2016-10-09 10:59:26 +0900

    libzip: Do not use CMake

--- mk/libzip.mk
+++ mk/libzip.mk
@@ -1,11 +1,10 @@
-libzip_CONFIGURE = CMake
-libzip_CONFIGURE_FLAGS = -DCMAKE_C_FLAGS=-DZIP_STATIC
+libzip_CHECKPOINT = $(DEPENDENCY_PREFIX)/lib/libzip.a
+libzip_CONFIGURE = script
+libzip_CONFIGURE_FLAGS = --disable-shared CPPFLAGS=-DZIP_STATIC
 libzip_SRC = https://github.com/vitasdk-experiment/libzip.git
 libzip_SRC_REF = vita
 libzip_TYPE = dependency
 ifdef BUILD_DEPENDENCY
 libzip_CONFIGURE_DEPENDENCY = prepare-weak-zlib
-libzip_CONFIGURE_FLAGS += \
-	-DZLIB_INCLUDE_DIR=$(DEPENDENCY_PREFIX)/include \
-	-DZLIB_LIBRARY=$(DEPENDENCY_PREFIX)/lib/libz.a
+libzip_CONFIGURE_FLAGS += --with-zlib=$(DEPENDENCY_PREFIX)
 endif
